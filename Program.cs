fda7d7c9aa9b39833fb8d37156f6beb06cf93d35
george.scott <george.scott@sage.com>
2021-04-28 14:37:11 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RuleExpressionParserTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string testExpression = "Tenant Must Have FeatureAllowanceCounter[Product_Users] <= 5";
+
+            ExpressionParser parser = new ExpressionParser();
+
+            var result = parser.IsValid(testExpression);
+
+            var message = result ? "Allowed" : "Not allowed";
+
+            Console.WriteLine(message);
+        }
+    }
+
+    public class ExpressionParser
+    {
+        Regex regex = 
+            new Regex("^(?'Bucket'(Account|Business|Tenant))\\s(?'Constraint'(Must Have|Must Not Have))\\s(?'ContextKey'(Product|Module|Feature|FeatureAllowanceCounter))\\[(?'ContextValue'.*)\\]\\s(?'ConditionOperator'(=|>|>=|<|<=))\\s(?'ConditionValue'([0-9]|[1-9][0-9]|[1-9][0-9][0-9]|[1-9][0-9][0-9][0-9]))$");
+
+        public bool IsValid(string expression)
+        {
+            Match match = regex.Match(expression);
+
+            if (match.Success)
+            {
+                Console.WriteLine(match.Groups["Bucket"].Value);
+            }
+            return false;
+        }
+
+    }
+}
